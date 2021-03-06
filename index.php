ef7e13bfb93354a67abacdff339289a86362ef4d
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-06 18:28:50 -0300

    Separação PHP

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php require_once('header.php') ?>
+
+<div class="destaque wide">
+    <div class="destaque-conteudo">
+        <h1 class="main-title">Pousada Secreta</h1>
+        <p class="subtitle">A pousada reservada em Angra dos Reis</p>
+    </div>
+</div>
+
+<div class="services">
+    
+    <div class="services-text">
+        <h2 class="section-title">Seja Bem vindo(a)!</h2>
+        <p class="section-text">Relaxe em nossas acomodações e curta o melhor de Angra dos Reis.</p>
+        <p class="section-text">Temos quarto para solteiro, casal ou família.</p>
+    </div>
+
+    <div class="services-pics">
+        <figure>
+            <img src="assets/quarto-solteiro1.jpg" alt="">
+            <figcaption>Quarto de Solteiro</figcaption>
+        </figure>
+        <figure>
+            <img src="assets/quarto-casal1.jpg" alt="">
+            <figcaption>Quarto de Casal</figcaption>
+        </figure>
+        <figure>
+            <img src="assets/quarto-familia1.jpg" alt="">
+            <figcaption>Quarto de Família</figcaption>
+        </figure>
+    </div>
+</div>
+
+<div class="rota" id="rota">
+    <h2 class="section-title">Rota</h2>
+    <p class="section-text">Veja como chegar até a pousada.</p>
+    
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115149.61120482566!2d-54.534486121592664!3d-25.590753786942113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f690f505555555%3A0xcf87e0b9bf1a020f!2sAeroporto%20Internacional%20de%20Foz%20do%20Igua%C3%A7u%20-%20Cataratas!5e0!3m2!1spt-BR!2sbr!4v1614184130829!5m2!1spt-BR!2sbr"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+<div class="sobre" id="sobre">
+    <div class="sobre-titulo">
+        <h1 class="section-title">Sobre a pousada!</h1>
+        <p class="section-text">Conheça um pouco sobre nossa pousada</p>
+    </div>
+
+    <div class="sobre-cards container">
+        <div class="card">
+            <div class="card-text">
+                <h2 class="card-title">A pousada</h2>
+                <p class="card-info">A pousada dev dispõe de estacionamento privativo gratuito, piscina ao ar livre e bar</p>
+            </div>
+            <img class="card-img" src="assets/img1.jpg" alt="">
+        </div>
+        <div class="card">
+            <div class="card-text">
+                <h2 class="card-title">Alimentação</h2>
+                <p class="card-info">A pousada serve café da manhã em estilo continental ou buffet. Nossos restaurantes funcionam 24 horas</p>
+            </div>
+            <img class="card-img" src="assets/img2.jpg" alt="">
+        </div>
+        <div class="card">
+            <div class="card-text">
+                <h2 class="card-title"> Nossos quartos</h2>
+                <p class="card-info">Os quartos possuem TV de tela plana a cabo, cozinha compacta e área de refeições. Além disso, as unidades
+                da Pousada Dev contam com ar-condicionado e guarda-roupa</p>
+            </div>
+            <img class="card-img" src="assets/img3.jpg" alt="">
+        </div>
+        <div class="card">
+            <div class="card-text">
+                <h2 class="card-title">Recepção</h2>
+                <p class="card-info">Nossa recepção fica aberta 24 horas</p>
+            </div>
+            <img class="card-img" src="assets/img5.jpg" alt="">
+        </div>
+    </div>
+
+</div>
+
+<?php require_once('footer.php') ?>
---
+++ quartos.php
@@ -0,0 +1,77 @@
+<?php require_once('header.php') ?>
+
+<img src="assets/img-fundo.jpg" class="destaque-quartos">
+
+<main class="main-rooms">
+<div class="single-rooms">
+    <h2 class="section-title">Quartos de Solteiro</h2>
+    <p class="section-text">Conheça nossos quartos de solteiro.</p>
+
+    <div class="rooms-pictures">
+        <img src="assets/quarto-solteiro1.jpg" alt="">
+        <img src="assets/quarto-solteiro2.jpg" alt="">
+        <img src="assets/quarto-solteiro3.jpg" alt="">
+    </div>
+
+    <p class="room-info-title" >Informações sobre o quarto</p>
+    <div class="room-info">
+        <ul class="room-details">
+            <li>TV de tela plana a cabo</li>
+            <li>Cozinha compacta</li>
+            <li>Área para refeições</li>
+            <li>Ar-condicionado</li>
+            <li>1 cama de casal e 1 de solteiro</li>
+            <li>Cancelamento grátis</li>
+        </ul>
+    </div>
+</div>
+<div class="family-rooms">
+    <h2 class="section-title">Quartos de Casal</h2>
+    <p class="section-text">Conheça nossos quartos de casal.</p>
+
+    <div class="rooms-pictures">
+        <img src="assets/quarto-casal1.jpg" alt="">
+        <img src="assets/quarto-casal2.jpg" alt="">
+        <img src="assets/quarto-casal3.jpg" alt="">
+        
+    </div>
+
+    <p class="room-info-title" >Informações sobre o quarto</p>
+    <div class="room-info">
+        
+        <ul class="room-details">
+            <li>TV de tela plana a cabo</li>
+            <li>Cozinha compacta</li>
+            <li>Área para refeições</li>
+            <li>Ar-condicionado</li>
+            <li>1 cama de casal e 1 de solteiro</li>
+            <li>Cancelamento grátis</li>
+        </ul>
+    </div>
+</div>
+<div class="family-rooms">
+    <h2 class="section-title">Quartos de Família</h2>
+    <p class="section-text">Conheça nossos quartos família.</p>
+
+    <div class="rooms-pictures">
+        <img src="assets/quarto-familia1.jpg" alt="">
+        <img src="assets/quarto-familia2.jpg" alt="">
+        <img src="assets/quarto-familia3.jpg" alt="">
+        
+    </div>
+
+    <p class="room-info-title" >Informações sobre o quarto</p>
+    <div class="room-info">
+        <ul class="room-details">
+            <li>TV de tela plana a cabo</li>
+            <li>Cozinha compacta</li>
+            <li>Área para refeições</li>
+            <li>Ar-condicionado</li>
+            <li>1 cama de casal e 1 de solteiro</li>
+            <li>Cancelamento grátis</li>
+        </ul>
+    </div>
+</div>
+</main>
+
+<?php require_once('footer.php'); ?>
